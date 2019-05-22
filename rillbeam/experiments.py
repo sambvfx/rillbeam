b830c72834c94ff863422759332a981f5f8b3dea
Chad Dombrova <chadrik@gmail.com>
2019-05-21 23:48:37 -0700

    Allow custom options to be passed to pipeline

--- rillbeam/experiments.py
+++ rillbeam/experiments.py
@@ -1,4 +1,6 @@
 import sys
+import argparse
+import logging
 
 import apache_beam as beam
 from apache_beam import pvalue
@@ -8,10 +10,10 @@ import apache_beam.transforms.trigger as trigger
 import apache_beam.transforms.window as window
 
 
-def test_flowbased():
+def test_flowbased(argv):
     from rillbeam.components import Sleep, Log
 
-    pipeline_options = PipelineOptions()
+    pipeline_options = PipelineOptions(argv)
     pipeline_options.view_as(SetupOptions).save_main_session = True
     pipe = beam.Pipeline(options=pipeline_options)
 
@@ -28,16 +30,20 @@ def test_flowbased():
 
 if __name__ == '__main__':
 
+    logging.getLogger().setLevel(logging.INFO)
+
     tests = {k: v for k, v in globals().items()
              if k.startswith('test') and callable(v)}
 
-    fltr = sys.argv[1:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filter')
+    known_args, pipeline_args = parser.parse_known_args()
 
     for name, func in tests.items():
-        if fltr and name not in fltr:
+        if known_args.filter and name not in known_args.filter:
             continue
         print
         print '-' * 80
         print name
         print '-' * 80
-        func()
+        func(pipeline_args)
